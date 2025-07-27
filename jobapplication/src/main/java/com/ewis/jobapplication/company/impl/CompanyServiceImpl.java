89131b4337b1c4b4278f6c31be787edce9cef3d7
Akila <akilaewis@gmail.com>
2025-07-27 14:19:57 +0530

    Added the rest controller function to CompanyController - getAllCompanies

--- jobapplication/src/main/java/com/ewis/jobapplication/company/impl/CompanyServiceImpl.java
+++ jobapplication/src/main/java/com/ewis/jobapplication/company/impl/CompanyServiceImpl.java
@@ -3,9 +3,11 @@ package com.ewis.jobapplication.company.impl;
 import com.ewis.jobapplication.company.Company;
 import com.ewis.jobapplication.company.CompanyRepository;
 import com.ewis.jobapplication.company.CompanyService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CompanyServiceImpl implements CompanyService {
 
     private CompanyRepository companyRepository;
