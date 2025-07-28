5a4c0f51141836c0fcddc06ca61d475606b0a5fb
Akila <akilaewis@gmail.com>
2025-07-28 21:34:45 +0530

    Update a review added

--- jobapplication/src/main/java/com/ewis/jobapplication/review/ReviewController.java
+++ jobapplication/src/main/java/com/ewis/jobapplication/review/ReviewController.java
@@ -35,4 +35,15 @@ public class ReviewController {
     public ResponseEntity<Review> getReview (@PathVariable Long companyId, @PathVariable Long reviewId){
         return new ResponseEntity<>(reviewService.getReview(companyId, reviewId), HttpStatus.OK);
     }
+
+    @PutMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> updateReview(@PathVariable Long companyId, @PathVariable Long reviewId, @RequestBody Review review){
+        boolean isReviewUpdated = reviewService.updateReview(companyId, reviewId, review);
+        if (isReviewUpdated){
+            return new ResponseEntity<>("Review updated successfully", HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("Review not updated", HttpStatus.NOT_FOUND);
+        }
+
+    }
 }
