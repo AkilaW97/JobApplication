b699ef9286e00accffcea64ff3b9e00c727d3faa
Akila <akilaewis@gmail.com>
2025-07-28 11:02:47 +0530

    Added Get all the reviews using companyID

--- jobapplication/src/main/java/com/ewis/jobapplication/review/ReviewService.java
+++ jobapplication/src/main/java/com/ewis/jobapplication/review/ReviewService.java
@@ -1,4 +1,7 @@
 package com.ewis.jobapplication.review;
 
+import java.util.List;
+
 public interface ReviewService {
+    List<Review> getAllReviews(Long companyId);
 }
--- jobapplication/src/main/java/com/ewis/jobapplication/review/impl/ReviewServiceImpl.java
+++ jobapplication/src/main/java/com/ewis/jobapplication/review/impl/ReviewServiceImpl.java
@@ -1,7 +1,24 @@
 package com.ewis.jobapplication.review.impl;
 
+import com.ewis.jobapplication.review.Review;
+import com.ewis.jobapplication.review.ReviewRepository;
+import com.ewis.jobapplication.review.ReviewService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ReviewServiceImpl {
+public class ReviewServiceImpl implements ReviewService {
+
+    private ReviewRepository reviewRepository;
+
+    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public List<Review> getAllReviews(Long companyId) {
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews;
+    }
 }
